09778ccda0f887d36515695f4f2da0cc1eadd75e
jerrylei98 <jerrylei98@gmail.com>
2015-11-06 14:04:01 -0500

    downloads photos locally

--- clist.py
+++ clist.py
@@ -1,10 +1,12 @@
 import ast
 import urllib2
 import urllib
+import os
 #import json
 #from BeautifulSoup import BeautifulSoup
 #url = "http://newyork.craigslist.org/brx/boa/5293603888.html"
 
+links = []
 def cmake(a):
     url = a
     resp = urllib2.urlopen(url)
@@ -37,14 +39,19 @@ def cmake(a):
     while i < len(ree):
         ree[i] = ast.literal_eval(ree[i])
         i+=1
-    i = 0
 
-    
-    global links[]
+    i = 0    
+    global links
     while i < len(ree):
         links.append(ree[i].get('url'))
         i+=1
-    
+
+    i = 0
+    while i < len(links):
+        urllib.urlretrieve(links[i], str(i) + '.jpg')
+        i+=1
+
+            
 #vals = ast.literal_eval(ree)
 
 def returnTitle():
@@ -54,10 +61,10 @@ def returnText():
 def returnLinks():
     return links
         
-#cmake("http://newyork.craigslist.org/lgi/cto/5303380345.html")
-#print "Title:" + returnTitle() + "\n"
-#print "Text:" + returnText() + "\n"
-#print "Image Links:" + str(returnLinks())
+cmake("http://newyork.craigslist.org/lgi/cto/5303380345.html")
+print "Title:" + returnTitle() + "\n"
+print "Text:" + returnText() + "\n"
+print "Image Links:" + str(returnLinks())
     
 
 
