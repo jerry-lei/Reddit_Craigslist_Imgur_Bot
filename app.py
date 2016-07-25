9b8a5c729a3020ded2f70032b3843526c215f1d0
jerrylei98 <jerrylei98@gmail.com>
2016-07-25 17:33:30 -0400

    organized post/db functions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import time
 import praw
 import working
+import database
 from api_keys import reddit_clientid, reddit_clientsecret, reddit_user, reddit_pass
 
 r = praw.Reddit('Craiglist bot by /u/jerrylei98')
@@ -8,18 +9,20 @@ r = praw.Reddit('Craiglist bot by /u/jerrylei98')
                      #client_secret = reddit_clientsecret,
                      #redirect_uri='http://162.243.63.270')
 r.login(reddit_user, reddit_pass)
-finished = []
 
 while True:
+    print '*Awake*'
     subreddit = r.get_subreddit('jerrylei98')
     for submission in subreddit.get_new(limit=10):
         link = submission.url
-        if 'craigslist.org' in link and submission.id not in finished:
+        if 'craigslist.org' in link and database.checkDB(submission.id):
             try:
                 fin = working.imgur_album(link)
-                submission.add_comment(fin[0] + " - " + fin[1] + "\n\n[LINK TO IMGUR](https://" + fin[3] + ")")
-                print "------- LINK: https://" + fin[3]
-                finished.append(submission.id)
+                database.intoDB(submission.id, fin[3])
+                submission.add_comment(fin[0] + " - " + fin[1]
+                                       + "\n\n**[LINK TO IMGUR](https://imgur.com/a/" + fin[3] + ")**\n\n>"
+                                       + fin[2])
+                print "------- LINK: https://imgur.com/a/" + fin[3]
             except(IndexError):
                 print "NOT POST LINK"
                 pass
