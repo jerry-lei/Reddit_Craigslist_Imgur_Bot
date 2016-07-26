50a992b41f489e63f35d502db7ae6d683c63015c
jerrylei98 <jerrylei98@gmail.com>
2016-07-26 19:43:29 -0400

    db functions

--- database.py
+++ database.py
@@ -7,11 +7,29 @@ def intoDB(sub_id, album_id):
     conn.commit()
     conn.close()
 
-def checkDB(sub_id):
+def intoDB2(sub_id):
     conn = sqlite3.connect('log.db')
     c = conn.cursor()
-    t = (sub_id,)
-    c.execute('SELECT * FROM cl_log WHERE submission_id=?',t)
+    c.execute("INSERT INTO sub_log VALUES(?)", [sub_id])
+    conn.commit()
+    conn.close()
+
+def intoDB3(com_id):
+    conn = sqlite3.connect('log.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO com_log VALUES(?)", [com_id])
+    conn.commit()
+    conn.close()
+
+def checkDB(sub_id, table):
+    conn = sqlite3.connect('log.db')
+    c = conn.cursor()
+    if table == "sub_log":
+        c.execute('SELECT * FROM sub_log WHERE submission_id=?', [sub_id])
+    elif table == "cl_log":
+        c.execute('SELECT * FROM cl_log WHERE submission_id=?', [sub_id])
+    elif table == "com_log":
+        c.execute('SELECT * FROM com_log WHERE comment_id=?', [sub_id])
     if c.fetchone() == None:
         conn.close()
         return True
