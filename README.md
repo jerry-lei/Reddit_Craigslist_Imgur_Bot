799aad0bdd11e5d0515f503e4398dddf3f4bcfb9
jerrylei98 <jerrylei98@gmail.com>
2016-07-26 19:44:23 -0400

    README update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # clbot
 Craiglist bot to learn how to implement APIs. Testing on: [/r/jerrylei98](https://reddit.com/r/jerrylei98)
+
+### Log
+
+- (July 25) Replies to link submission
+
+- (July 26) Added text reply for comment/submission text -- multiple imgur albums
--- clist_img.py
+++ clist_img.py
@@ -1,35 +1,42 @@
 import urllib2
+from re import findall
+
+def find_links(stringy):
+    return findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', stringy)
 
 def cmake(CL_link):
     url = CL_link
-    resp = urllib2.urlopen(url)
-    ree = resp.read()
+    try:
+        resp = urllib2.urlopen(url)
+        ree = resp.read()
 
-    title = ree
-    title = title.split("<title>")[1]
-    title = title.split("</title>")[0]
+        title = ree
+        title = title.split("<title>")[1]
+        title = title.split("</title>")[0]
 
-    price = ree
-    price = price.split('<span class="price">')[1]
-    price = price.split('</span>')[0]
+        price = ree
+        price = price.split('<span class="price">')[1]
+        price = price.split('</span>')[0]
 
-    summary = ree
-    summary = summary.split('"postingbody">')[1]
-    summary = summary.split('</section>')[0]
-    summary = summary.replace('<br>','\n>')
-    if "showcontact" in summary:
-        summary = summary.split('<a href=')[0]
+        summary = ree
+        summary = summary.split('"postingbody">')[1]
+        summary = summary.split('</section>')[0]
+        summary = summary.replace('<br>','\n>')
+        if "showcontact" in summary:
+            summary = summary.split('<a href=')[0]
 
-    img_links = ree
-    img_links = img_links.split('var imgList =')[1]
-    img_links = img_links.split('--></script>')[0]
-    img_links = img_links.replace('"','') #strips quotes
-    img_list = img_links.split(",")
-    global fin_list
-    fin_list = []
+        img_links = ree
+        img_links = img_links.split('var imgList =')[1]
+        img_links = img_links.split('--></script>')[0]
+        img_links = img_links.replace('"','') #strips quotes
+        img_list = img_links.split(",")
+        global fin_list
+        fin_list = []
 
-    for c1 in xrange(len(img_list)):
-        if "url" in img_list[c1]:
-            fin_list.append(img_list[c1].split('url:')[1])
-
-    return [title,price,summary,fin_list]
+        for c1 in xrange(len(img_list)):
+            if "url" in img_list[c1]:
+                fin_list.append(img_list[c1].split('url:')[1])
+                
+        return [title,price,summary,fin_list]
+    except:
+        return ''
